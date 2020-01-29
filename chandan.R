9b847f838e2b89edb92ab104efa4ee3abe55be44
20171cse0157 <60281283+20171cse0157@users.noreply.github.com>
2020-01-29 15:02:00 +0530

    Add files via upload
    29\01\2020

---
+++ chandan.R
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Resume</title>
+</head>
+<body>
+<h1 align="center">chandan.R<hr></h1>
+<p align="center">Anchorage,Ak *(555)67868*chandan313@gmail.com. </p>
+<h2>Professional Summary</h2>
+<p>With extensive experience in the commercial utilisation of multiple social media channels including Facebook, Twitter and YouTube; I build successful social strategies that increase brand awareness, promote customer engagement and ultimately drive web traffic and conversions. With a degree in psychology and strong business acumen, I bring a customer focused approach to online and social marketing.</p>
+<h2>Work History</h2>
+<p><b>Senior machanizer</b>-10/12/2016 <p>
+<p><b>Hallmark</b>-Anchorage,Ak</p>
+<ul>
+<li>With 15+ years of experience in python programming</li>
+<li>Proficient in standard UX and abcdef software</li>
+<li>With specific knowledge of interface patterns for mobile.</li>
+<li>With good understanding of HTML,python,C,JAVA</li>
+</ul>
+<p><b>Machanizer</b>-30/1/2016 <p>
+<p><b>Hallmark</b>-Anchorage,Ak</p>
+<ul>
+<li>Marketing Intern, Resume Revolution Inc., Cunningham Springs, CO, 2015 to 2016.Designed the content management strategy which increased readership by 60% in 2 months.Increased revenue by 12% over 6 months</li>
+<li>Volunteer Marketer, Charity Books Store, Portland, OR, 2010 to 2013  Prepared 30+ monthly newsletters.Created and administered the store and web site.Visual merchandising.</li>
+<li>Writer and Translator, Freelance, Portland, OR, 2010 to 2015 - Wrote 500+ blog articles for clients from tech and food industries.</li>
+</ul>
+<p><b>Customer services</b>-3/12/2016 <p>
+<p><b>Hallmark</b>-Anchorage,Ak</p>
+<ul>
+<li>Trader Joe's. I've always been a big fan of Trader Joe's, which, if you're unfamiliar, is a specialty and privately owned grocery chain</li>
+<li>Rackspace.</li>
+<li> Great service comes to us across all industries</li>
+<li>Carlton</li>
+</ul>
+<h2>Skills</h2>
+<ul>
+<li>ghi.</li>
+<li>team.</li>
+<li>Making.</li>
+<li>Time Management.</li>
+<li>Self.</li>
+<li>Resolution.</li>
+</ul>
+<h2>Education</h2>
+</ul>
+<li>sslc</li>
+<li>PU</li>
+<li>B.tech in CSE</li>
+</ul>
+</body>
+</html>
